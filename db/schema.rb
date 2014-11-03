0e0c0a1b793eca95525d35542499bc0e60e6994b
craig.rettew <craig@zeptoindustries.com>
2014-11-03 10:50:35 -0500

    Cleaned up the pins scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141031143144) do
+ActiveRecord::Schema.define(version: 20141101195659) do
 
   create_table "events", force: true do |t|
     t.string   "name"
     t.datetime "start_at"
     t.datetime "end_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
