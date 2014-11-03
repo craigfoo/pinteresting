5a068f5022bdbd96ee20406b222abdec4a11d7e4
craig.rettew <craig@zeptoindustries.com>
2014-11-03 11:52:52 -0500

    Add pins/users association

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index]
 
   # GET /pins
   # GET /pins.json
@@ -14,7 +16,7 @@ class PinsController < ApplicationController
 
   # GET /pins/new
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
   # GET /pins/1/edit
@@ -24,11 +26,11 @@ class PinsController < ApplicationController
   # POST /pins
   # POST /pins.json
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.'        
     else
-      format.html { render :new }    
+      render actions: 'new'   
     end
   end
 
@@ -38,7 +40,7 @@ class PinsController < ApplicationController
     if @pin.update(pin_params)
       redirect_to @pin, notice: 'Pin was successfully updated.'        
     else
-      format.html { render :edit }        
+      render actions: 'edit'
     end
   end
 
@@ -46,15 +48,18 @@ class PinsController < ApplicationController
   # DELETE /pins/1.json
   def destroy
     @pin.destroy
-    respond_to do |format|
-      redirect_to pins_url, notice: 'Pin was successfully destroyed.'
-    end
+    redirect_to pins_url, notice: 'Pin was successfully destroyed!'       
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
       @pin = Pin.find(params[:id])
+    end
+
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: 'Not authorized to edit this pin' if @pin.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141101195659) do
+ActiveRecord::Schema.define(version: 20141103160314) do
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -25,7 +25,10 @@ ActiveRecord::Schema.define(version: 20141101195659) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
